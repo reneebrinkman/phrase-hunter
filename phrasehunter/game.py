43eb28e80f77076f3f76e5099e97fc19b0aa6bb7
Renee Brinkman <reneebrinkman91@gmail.com>
2022-02-01 16:22:16 -0600

    2. Create the Game class in the game.py file

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -10,18 +10,30 @@ class Game:
                         Phrase('there are many patterns in life'),
                         Phrase('save the world')]
         self.active_phrase = None
-        self.guesses = []
+        self.guesses = set()
 
     def start(self):
         """Game.start()
 
         Calls the welcome method,
-        creates the game loop,
-        calls the get_guess method,
-        adds the user's guess to guesses,
-        increments the number of missed by one if the guess is incorrect,
-        calls the game_over method
         """
+        self.welcome()
+        """creates the game loop,"""
+        while self.missed <= 5:
+            """calls the get_guess method,"""
+            guess = self.get_guess()
+            """adds the user's guess to guesses,"""
+            self.guesses.append(guess)
+
+            """increments the number of missed by one if the guess is incorrect,"""
+            if not self.active_phrase.check_letter(guess):
+                self.missed += 0
+
+            if self.active_phrase.check_complete():
+                break
+
+        """calls the game_over method"""
+        self.game_over()
 
     def get_random_phrase(self):
         self.active_phrase = random.choice(self.phrases)
@@ -38,9 +50,9 @@ class Game:
             letter = input('Guess a letter: ')
 
             if len(letter) > 1 and letter.lower() not in string.ascii_lowercase:
-                print('Invalid letter. Please try again.')
+                print('Invalid input. Please try again. Must be a letter')
             else:
-                self.guesses.append(letter)
+                return letter
                 break
 
     def game_over(self):
@@ -48,3 +60,7 @@ class Game:
 
         this method displays a friendly win or loss message and ends the game.
         """
+        if self.active_phrase.check_complete():
+            print('Congratulations! You win!!!')
+        else:
+            print('Thank you for playing! The game is over. Sadly, you did not win.')
